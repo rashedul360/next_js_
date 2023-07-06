f3bec51ce9cf6e08322ccfc81e0f9da05d3049e9
Rashedul islam <rashedul360rs@gmail.com>
2023-07-06 15:37:59 +0600

    added some code

--- app/posts/[id]/loading.tsx
+++ app/posts/[id]/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const loading = () => {
  return (
   <div className="text-center bg-green-200 p-100">
-   <p className="text-4xl">loading----</p>
+   <p className="text-4xl">loading ....</p>
   </div>
  );
 };
